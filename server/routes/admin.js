fb5bf6ed0f0e0eb65c64f44312c148e76e60d11e
David Carlisle <dave_0021@yahoo.com>
2020-06-30 12:06:43 -0500

    server-setup

---
+++ server/routes/admin.js
@@ -0,0 +1,32 @@
+let express = require("express");
+let router = express.Router();
+let models = require("../models");
+
+router.get("/", (req, res) => {
+  res.render("admin");
+});
+
+router.post("/", (req, res) => {
+  let image = req.body.bookImage;
+  let title = req.body.bookTitle;
+  let author = req.body.bookAuthor;
+  let category = req.body.bookCategory;
+  let description = req.body.bookDescription;
+  let isbn_10 = req.body.bookISBN_10;
+  let price = req.body.bookPrice;
+  let product = models.Product.build({
+    image: image,
+    title: title,
+    author: author,
+    category: category,
+    description: description,
+    isbn_10: isbn_10,
+    price: price,
+  });
+  // console.log(product);
+  product.save().then(() => {
+    res.render("admin", { title: title });
+  });
+});
+
+module.exports = router;
---
+++ server/routes/register.js
@@ -0,0 +1,25 @@
+let express = require("express");
+let router = express.Router();
+let models = require("../models");
+var bcrypt = require("bcryptjs");
+
+// GET route to display registration page
+router.get("/", (req, res) => {
+  res.render("register");
+});
+// POST route to register a new user account
+router.post("/", (req, res) => {
+  let username = req.body.username;
+  let password = bcrypt.hashSync(req.body.password, 10);
+  let user = models.User.build({ username: username, password: password });
+  user
+    .save()
+    .then((savedUser) => {
+      res.redirect("/");
+    })
+    .catch(() => {
+      res.render("register", { message: "Username is already registered." });
+    });
+});
+
+module.exports = router;
