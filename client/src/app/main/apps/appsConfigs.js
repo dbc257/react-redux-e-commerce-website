3334392f1913d37f0062bc26b80f6efef9fc2775
David Carlisle <dave_0021@yahoo.com>
2020-07-03 02:00:32 -0500

    heroku

--- client/src/app/main/apps/appsConfigs.js
+++ client/src/app/main/apps/appsConfigs.js
@@ -1,5 +1,3 @@
-// import AnalyticsDashboardAppConfig from '../../../../../../Redux-Misc/dashboards/analytics/AnalyticsDashboardAppConfig';
-// import ProjectDashboardAppConfig from './dashboards/project/ProjectDashboardAppConfig';
 import ECommerceAppConfig from './e-commerce/ECommerceAppConfig';
 
 const appsConfigs = [ECommerceAppConfig];
